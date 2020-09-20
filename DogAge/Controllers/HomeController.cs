df4eaec56e8b93f0ee69b17c69d9a8f220e1701f
Esther Leonard <53614527+thewarpdrive84@users.noreply.github.com>
2020-09-20 17:30:27 +0100

    Add project files.

---
+++ DogAge/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DogAgeCalculator.Models;
+
+namespace DogAgeCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Calculate()
+        {
+            // no data displayed initially
+            return View();
+        }
+
+        // process data, display Age and Size
+        [HttpPost]
+        public ActionResult Calculate(Dog dog)
+        {
+            return View(dog);
+        }
+    }
+}
---
+++ DogAge/Models/Dog.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DogAgeCalculator.Models
+{
+    public class Dog
+    {
+        // imperial units (pounds)
+        const double SmallLimit = 20;
+        const double MediumLimit = 50;
+
+        // conversion factors from dog to human age
+        const double ConvertSmall = 4;
+        const double ConvertMedium = 6;
+        const double ConvertLarge = 8;
+
+        [Required]
+        [Display(Name = "Weight in Pounds")]
+        [Range(1, 400, ErrorMessage = "Pounds must be between 1 and 400")]
+        public double WeightPounds { get; set; }
+
+        [Required]
+        [Display(Name = "Age in Years")]
+        [Range(1, 30, ErrorMessage = "Age must be between 1 and 30")]
+        public double AgeYears { get; set; }
+
+        // calculate equivalent Human Age
+        [Display(Name = "so your dog's human age is")]
+        public double AgeValue
+        {
+            get
+            {
+                // do conversions to human years
+                if (WeightPounds <= SmallLimit)
+                {
+                    double dogHumanAge = AgeYears * ConvertSmall;
+                    return dogHumanAge;
+                }
+                else if (WeightPounds <= MediumLimit)
+                {
+                    double dogHumanAge = AgeYears * ConvertMedium;
+                    return dogHumanAge;
+                }
+                else
+                {
+                    double dogHumanAge = AgeYears * ConvertLarge;
+                    return dogHumanAge;
+                }
+            }
+        }
+
+        // show size category
+        [Display(Name ="Your dog is ")]
+        public string SizeCategory
+        {
+            get
+            {
+                if (WeightPounds <= SmallLimit)
+                {
+                    return "small,";
+                }
+                else if (WeightPounds <= MediumLimit)
+                {
+                    return "medium,";
+                }
+                else
+                {
+                    return "large,";
+                }
+            }
+        }
+    }
+}
